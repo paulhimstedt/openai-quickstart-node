eece00cde03a7ed55e87542ef2e3be2545cf4190
paulhimstedt <71269641+paulhimstedt@users.noreply.github.com>
2023-01-11 16:21:39 +0100

    V001
    framework and api connection functional

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,57 +1,117 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-export default async function (req, res) {
-  if (!configuration.apiKey) {
-    res.status(500).json({
-      error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
-    });
-    return;
-  }
-
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
-    res.status(400).json({
-      error: {
-        message: "Please enter a valid animal",
-      }
-    });
-    return;
-  }
-
-  try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
-    });
-    res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
-    // Consider adjusting the error handling logic for your use case
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
-    } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
-        error: {
-          message: 'An error occurred during your request.',
-        }
-      });
+export default async function(req, res) {
+    if (!configuration.apiKey) {
+        res.status(500).json({
+            error: {
+                message: "OpenAI API key not configured, please follow instructions in README.md",
+            }
+        });
+        return;
     }
-  }
+
+    const sender = req.body.sender || '';
+    const reciever = req.body.reciever || '';
+    const keyInfo = req.body.keyInfo || '';
+
+    if (sender.trim().length === 0) {
+        res.status(400).json({
+            error: {
+                message: "Please enter a valid animal",
+            }
+        });
+        return;
+    }
+
+    try {
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: generatePrompt(sender, reciever, keyInfo),
+            temperature: 0.6,
+            max_tokens: 300,
+        });
+        console.log(completion.data.choices[0].text)
+        res.status(200).json({ result: completion.data.choices[0].text });
+    } catch (error) {
+        // Consider adjusting the error handling logic for your use case
+        if (error.response) {
+            console.error(error.response.status, error.response.data);
+            res.status(error.response.status).json(error.response.data);
+        } else {
+            console.error(`Error with OpenAI API request: ${error.message}`);
+            res.status(500).json({
+                error: {
+                    message: 'An error occurred during your request.',
+                }
+            });
+        }
+    }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+function generatePrompt(sender, reciever, keyInfo) {
+    const capitalizedSender = sender[0].toUpperCase() + sender.slice(1).toLowerCase();
+    const capitalizedReciever = reciever[0].toUpperCase() + reciever.slice(1).toLowerCase();
+    const capitalizedKeyInput = keyInfo[0].toUpperCase() + keyInfo.slice(1).toLowerCase();
+    return `Write an email including the following Informations: 
+  Sender: ${capitalizedSender} 
+  Reciever: ${capitalizedReciever}
+  Key informations: ${capitalizedKeyInput} `;
+}
+/*process.on('unhandledRejection', (reason, promise) => {
+    console.log('Unhandled Rejection at:', reason.stack || reason)
+    // Recommended: send the information to sentry.io
+    // or whatever crash reporting service you use
+  });
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function(req, res) {
+
+    if (!configuration.apiKey) {
+        res.status(500).json({
+            error: {
+                message: "OpenAI API key not configured, please follow instructions in README.md",
+            }
+        });
+        return;
+    }
+        const sender = req.body.sender || '';
+        const receiver = req.body.receiver || '';
+        const key_info = req.body.key_info || '';
+        const animal = req.body.animal || '';
+        console.log(sender, receiver, key_info)
+
+
+
+
+
+    try {
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: generatePrompts(animal),
+            temperature: 0.6,
+        });
+        res.status(200).json({ result: completion });
+    } catch (error) {
+        if (response.status !== 200) {
+            throw new Error(`Request failed with status ${response.status}`);
+          }
+    }
+}
+
+function generatePrompts(animal) {
+    const capitalizedAnimal =
+        animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+    return `Suggest three names for an animal that is a superhero.
 
 Animal: Cat
 Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
@@ -60,3 +120,11 @@ Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
 Animal: ${capitalizedAnimal}
 Names:`;
 }
+
+function generatePrompt(senderInput, receiverInput, keyInput) {
+    return `Write an email including the following Informations:
+Sender: ${senderInput}
+Receiver:${receiverInput}
+Key Content Informations:${keyInput}`;
+}
+*/
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [senderInput, setSenderInput] = useState("");
+  const [recieverInput, setRecieverInput] = useState("");
+  const [keyInfoInput, setKeyInfoInput] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
@@ -14,16 +16,18 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({sender : senderInput, reciever: recieverInput, keyInfo: keyInfoInput}),
       });
 
       const data = await response.json();
       if (response.status !== 200) {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
-
+      console.log(data.result)
       setResult(data.result);
-      setAnimalInput("");
+      setSenderInput("");
+      setRecieverInput("");
+      setKeyInfoInput("");
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
@@ -44,10 +48,24 @@ export default function Home() {
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="sender"
+            placeholder="Enter an sender"
+            value={senderInput}
+            onChange={(e) => setSenderInput(e.target.value)}
+          />
+          <input
+            type="text"
+            name="reciever"
+            placeholder="Enter an Reciever"
+            value={recieverInput}
+            onChange={(e) => setRecieverInput(e.target.value)}
+          />
+          <input
+            type="text"
+            name="keyInfo"
+            placeholder="Provide key informations"
+            value={keyInfoInput}
+            onChange={(e) => setKeyInfoInput(e.target.value)}
           />
           <input type="submit" value="Generate names" />
         </form>
@@ -56,3 +74,118 @@ export default function Home() {
     </div>
   );
 }
+/*import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+
+export default function Home() {
+  
+  const [senderInput, setSenderInput] = useState("");
+  const [receiverInput, setReceiverInput] = useState("");
+  const [senderInput, setSenderInput] = useState("");
+  const [keyInput, setKeyInput] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      console.log("working");
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        
+        body: JSON.stringify({sender : senderInput}),
+      });
+
+      const data = await response.json();
+      
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      setResult(data.result);
+      
+     
+    } catch(error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>MailHelper</title>
+        <link rel="icon" href="/AIMAIL_icon.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/AIMAIL_icon.png" className={styles.icon} />
+        <h3>Generate custom Mails</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="sender"
+            placeholder="Enter an sender"
+            value={senderInput}
+            onChange={(e) => setSenderInput(e.target.value)}
+          />
+          <input type="submit" value="Generate names" />
+        </form>
+
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="receiver"
+            placeholder="enter reciever name"
+            value={receiverInput}
+            onChange={(e) => setReceiverInput(e.target.value)}
+          />
+          <input
+            type="text"
+            name="sender"
+            placeholder="enter sender name"
+            value={senderInput}
+            onChange={(e) => setSenderInput(e.target.value)}
+          />
+          <input
+            type="text"
+            name="key_info"
+            placeholder="enter key points"
+            value={keyInput}
+            onChange={(e) => setKeyInput(e.target.value)}
+          />
+          
+          <input type="submit" value="Generate Mail"/>
+         
+        </form>
+        
+
+        <div className={styles.result}>{result}</div>
+        
+      </main>
+    </div>
+  );
+}
+*/
+/*
+<div id="radio_input">
+            <label className="label cursor-pointer">
+              <span className="label-text">formal</span> 
+              <input type="radio" name="radio-10" className="radio checked:bg-red-500" checked/>
+            </label>
+            <label className="label cursor-pointer">
+              <span className="label-text">informal</span> 
+              <input type="radio" name="radio-10" className="radio checked:bg-red-500" checked/>
+            </label>            
+            <label className="label cursor-pointer">
+              <span className="label-text">humerous</span> 
+              <input type="radio" name="radio-10" className="radio checked:bg-red-500" checked/>
+            </label>
+          </div>
+          <input type="range" min="0" max="100" value="" className="range" />
+*/
