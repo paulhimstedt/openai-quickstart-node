8997c34983921e178f8dcd4a7fdfc12eaa1c8f7b
paulhimstedt <71269641+paulhimstedt@users.noreply.github.com>
2023-01-17 11:32:00 +0100

    dev2_001
    ...

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,10 @@ import styles from "./index.module.css";
 import * as React from 'react';
 import { NextUIProvider } from '@nextui-org/react';
 import { Input } from "@nextui-org/react";
-import { Card, Button,Text, Row ,Spacer, Grid} from "@nextui-org/react";
-
-
-import { Navbar, Link, Radio, useTheme, Progress } from "@nextui-org/react";
-
-
+import { Card, Button, Text, Row, Spacer, Grid } from "@nextui-org/react";
+import dynamic from 'next/dynamic'
+import { motion } from "framer-motion";
+import { Navbar, Link, Radio, useTheme, Progress, Popover } from "@nextui-org/react";
 
 
 
@@ -22,6 +20,7 @@ export default function Home() {
   const [humorousChecked, setHumorousChecked] = useState(false);
   const [result, setResult] = useState("");
   const [loadingState, setLoadingState] = useState();
+  var [x, setX] = useState(0);
 
   async function onSubmit(event) {
     event.preventDefault();
@@ -32,7 +31,7 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({sender : senderInput, reciever: recieverInput, keyInfo: keyInfoInput ,isFormal: formalChecked, isInformal: informalChecked, isHumorous: humorousChecked}),
+        body: JSON.stringify({ sender: senderInput, reciever: recieverInput, keyInfo: keyInfoInput, isFormal: formalChecked, isInformal: informalChecked, isHumorous: humorousChecked }),
       });
 
       const data = await response.json();
@@ -48,17 +47,17 @@ export default function Home() {
       setInformalChecked(false);
       setHumorousChecked(false);
       setLoadingState(false);
-    } catch(error) {
+    } catch (error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
     }
   }
- /*<img css={{mw:"10px"}} src="/AIMAIL_icon.PNG"/>*/
- const [variant, setVariant] = React.useState("default");
+  /*<img css={{mw:"10px"}} src="/AIMAIL_icon.PNG"/>*/
+  const [variant, setVariant] = React.useState("default");
   const [activeColor, setActiveColor] = React.useState("primary");
 
-  const {isDark} = useTheme();
+  const { isDark } = useTheme();
 
   const variants = [
     "default",
@@ -83,14 +82,15 @@ export default function Home() {
     "Sign Up",
   ];
   return (
-    
+
     <NextUIProvider>
-    <Navbar isBordered variant="sticky">
+
+      <Navbar isBordered variant="sticky">
         <Navbar.Brand>
           <Navbar.Toggle aria-label="toggle navigation" />
-         
+
           <Text b color="inherit" hideIn="xs">
-          AIMAIL
+            AIMAIL
           </Text>
         </Navbar.Brand>
         <Navbar.Content enableCursorHighlight hideIn="xs" variant="underline">
@@ -112,151 +112,176 @@ export default function Home() {
           </Navbar.Item>
         </Navbar.Content>
         <Navbar.Collapse>
-        {collapseItems.map((item, index) => (
-          <Navbar.CollapseItem key={item}>
-            <Link
-              color="inherit"
-              css={{
-                minWidth: "100%",
-              }}
-              href="#"
-            >
-              {item}
-            </Link>
-          </Navbar.CollapseItem>
-        ))}
-      </Navbar.Collapse>
-      </Navbar>  
-    <div>
-      <Head>
-        <title>AIMAIL</title>
-        <link rel="icon"  href="/AIMAIL_icon.PNG" />
-        
-      </Head>
-      <Grid.Container gap={2} justify="center" >
-      <Grid sm={12} md={5}>
-        <Card css={{ p:"$15" }}>
-          <Card.Header>
-            <Text b>Subject</Text>
-          </Card.Header>
-          <Card.Divider />
-          <Card.Body css={{ py: "$10" }}>
-            <p>{result.split(/<br ?\/?>/)
-          .flatMap((line, i) => [line, <br key={`line-${i}`} />])}</p>
+          {collapseItems.map((item, index) => (
+            <Navbar.CollapseItem key={item}>
+              <Link
+                color="inherit"
+                css={{
+                  minWidth: "100%",
+                }}
+                href="#"
+              >
+                {item}
+              </Link>
+            </Navbar.CollapseItem>
+          ))}
+        </Navbar.Collapse>
+      </Navbar>
+      <div>
+        <Head>
+          <title>AIMAIL</title>
+          <link rel="icon" href="/AIMAIL_icon.PNG" />
+
+        </Head>
+
+
+        <Grid.Container gap={2} justify="center"  >
+
+          <Grid sm={12} md={2.5} >
+            <div>
+              <motion.div
+                className="box"
+                animate={{ x }}
+                transition={{ type: "spring" }}
+              >
+              <Card css={{ p: "$15" }}>
+                <main >
+                  <Progress
+                    indeterminated={loadingState}
+                    color="secondary"
+                    status="secondary"
+                  />
+
+                  <Text
+                    h1
+                    size={60}
+                    css={{
+                      textGradient: "45deg, $blue600 -20%, $pink600 50%",
+                    }}
+                    weight="bold"
+                  >
+                    Generate Your Emails
+                  </Text>
+                  <form onSubmit={onSubmit}>
+                    <Spacer y={2} />
+                    <Input
+                      labelPlaceholder="enter sender"
+                      size="lg"
+                      placeholder="Large"
+                      status="default"
+                      type="text"
+                      name="sender"
+                      value={senderInput}
+                      onChange={(e) => setSenderInput(e.target.value)}
+                    />
+                    <Spacer y={2} />
+                    <Input
+                      type="text"
+                      name="reciever"
+                      labelPlaceholder="enter reciever"
+                      size="lg"
+                      placeholder="Large"
+                      status="default"
+                      value={recieverInput}
+                      onChange={(e) => setRecieverInput(e.target.value)}
+                    />
+                    <Spacer y={2} />
+                    <Input
+                      type="text"
+                      name="keyInfo"
+                      status="default"
+                      size="lg"
+                      placeholder="Large"
+                      labelPlaceholder="Provide key informations"
+                      value={keyInfoInput}
+                      onChange={(e) => setKeyInfoInput(e.target.value)}
+                    />
+                    <Spacer y={2} />
+                    <div >
+                      <label>
+                        <input
+                          type="checkbox"
+                          name="style"
+                          value="formal"
+                          checked={formalChecked}
+                          onChange={(e) => setFormalChecked({ formalChecked: e.target.checked })}
+                        />
+                        Formal
+                      </label>
+                      <label>
+                        <input
+                          type="checkbox"
+                          name="style"
+                          value="informal"
+                          checked={informalChecked}
+                          onChange={(e) => setInformalChecked({ informalChecked: e.target.checked })}
+                        />
+                        Informal
+                      </label>
+                      <label>
+                        <input
+                          type="checkbox"
+                          name="style"
+                          value="humorous"
+                          checked={humorousChecked}
+                          onChange={(e) => setHumorousChecked({ humorousChecked: e.target.checked })}
+                        />
+                        Humorous
+                      </label>
+                    </div>
+                    <Spacer y={1} />
+
+
+                    <Popover placement={"right-bottom"}>
+
+                      <Popover.Trigger>
+                        <Button shadow color="gradient"  type="submit" value="Generate names" > Generate mail</Button>
+
+                      </Popover.Trigger>
+
+                      <Popover.Content css={{ ml: "10%", h: "600px" }}>
+
+
+
+
+                        <Card.Header>
+                          <Text b>Subject</Text>
+                        </Card.Header>
+                        {result.split(/<br ?\/?>/)
+                          .flatMap((line, i) => [line, <br key={`line-${i}`} />])}
+
+
+
+                        <Card.Footer>
+                          <Row justify="flex-end">
+                            <Button size="sm" light>
+                              Regenerate
+                            </Button>
+                            <Button size="sm">Copy</Button>
+                          </Row>
+                        </Card.Footer>
+
+
+
+
+                      </Popover.Content>
+                    </Popover>
+                  </form>
+
+
+                </main>
+              </Card>
+              </motion.div>
+            </div>
             
-            
-          </Card.Body>
-          <Card.Divider />
-          <Card.Footer>
-            <Row justify="flex-end">
-              <Button size="sm" light>
-                Regenerate
-              </Button>
-              <Button size="sm">Copy</Button>
-            </Row>
-          </Card.Footer>
-        </Card>
-      </Grid>
-      <Grid sm={12} md={5} >
-        <Card css={{ p:"$15" }}>
-        <main >
-        <Progress
-          indeterminated={loadingState}
-          color="secondary"
-          status="secondary"
-        />
-          
-        <Text
-        h1
-        size={60}
-        css={{
-          textGradient: "45deg, $blue600 -20%, $pink600 50%",
-        }}
-        weight="bold"
-      >
-        Generate Your Emails
-      </Text>
-        <form onSubmit={onSubmit}>
-        <Spacer y={2} />
-          <Input
-            labelPlaceholder="enter sender" 
-            size="lg" 
-            placeholder="Large" 
-            status="default"
-            type="text"
-            name="sender"
-            value={senderInput}
-            onChange={(e) => setSenderInput(e.target.value)}
-          />
-           <Spacer y={2} />
-          <Input
-            type="text"
-            name="reciever"
-            labelPlaceholder="enter reciever" 
-            size="lg" 
-            placeholder="Large"
-            status="default"
-            value={recieverInput}
-            onChange={(e) => setRecieverInput(e.target.value)}
-          />
-           <Spacer y={2} />
-          <Input
-            type="text"
-            name="keyInfo"
-            status="default"
-            size="lg" 
-            placeholder="Large" 
-            labelPlaceholder="Provide key informations"
-            value={keyInfoInput}
-            onChange={(e) => setKeyInfoInput(e.target.value)}
-          />
-          <Spacer y={2} />
-          <div className={styles.checkboxGroup}>
-          <label>
-            <input
-              type="checkbox"
-              name="style"
-              value="formal"
-              checked={formalChecked}
-              onChange={(e) => setFormalChecked({ formalChecked: e.target.checked })}
-            />
-            Formal
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              name="style"
-              value="informal"
-              checked={informalChecked}
-              onChange={(e) => setInformalChecked({ informalChecked: e.target.checked })}
-            />
-            Informal
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              name="style"
-              value="humorous"
-              checked={humorousChecked}
-              onChange={(e) => setHumorousChecked({ humorousChecked: e.target.checked })}
-            />
-            Humorous
-          </label>
-          </div>
-          <Spacer y={1} />
-          
-          <Button shadow color="gradient" type="submit" value="Generate names" > Generate mail</Button>
-        </form>
-        
-        
-      </main>
-        </Card>
-      </Grid>
-    </Grid.Container>
-      <Row justify="center" align="center">
-      
-    </Row>
-    </div>
+          </Grid>
+
+
+
+        </Grid.Container>
+
+
+      </div>
     </NextUIProvider>
   );
 }
